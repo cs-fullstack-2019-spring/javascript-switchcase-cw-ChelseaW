f9528f6aa068911cdcc220fdee0d4f6d5dcab75e
ChelseaW <c.winfrey93@yahoo.com>
2019-01-16 12:02:08 -0600

    Switch

---
+++ Switch2.js
@@ -0,0 +1,43 @@
+// Put the start of your program in a main function.
+// Ask the user to enter a 3 digit binary number.
+//     Create a switch case that prints 1
+//     if you enter 1, 2
+//     if you enter 10, 3
+//     if you enter 11, 4
+//     if you enter 100, and 5
+//     if you enter 101.
+
+
+main()
+
+    function main()
+    {
+        var quest =  prompt("Enter a binary number:");
+        switch (quest)
+        {case "1":
+            console.log("2");
+            break;
+
+            case "10":
+                console.log("3");
+                break;
+
+            case "11":
+                console.log("4");
+                break;
+
+            case "100":
+                console.log("5");
+                break;
+
+            default:
+                console.log("");
+        }
+
+
+
+
+
+    }
+
+
---
+++ Switch3.js
@@ -0,0 +1,13 @@
+// Put the start of your program in a main function.
+// Create an attendance array. Ask the user if a teacher, student, or parent is checking in.
+// Use a switch statement to add the teacher/student/parent to the attendance array,
+//     then log the option they chose.
+//     Repeat this five times.
+
+
+main()
+
+function main()
+{
+
+}
